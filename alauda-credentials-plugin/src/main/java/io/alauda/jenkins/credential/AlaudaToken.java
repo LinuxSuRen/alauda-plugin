fce4fadb88e27db5c35730fd7fc35b1d1f77e5f5
Zhao Xiaojie <zxjlwt@126.com>
2018-10-23 16:28:31 +0800

    Init alauda jenkins plugin project

---
+++ alauda-credentials-plugin/src/main/java/io/alauda/jenkins/credential/AlaudaToken.java
@@ -0,0 +1,35 @@
+package io.alauda.jenkins.credential;
+
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.impl.BaseStandardCredentials;
+import hudson.Extension;
+import hudson.util.Secret;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class AlaudaToken extends BaseStandardCredentials {
+
+    private final Secret secret;
+
+    @DataBoundConstructor
+    public AlaudaToken(CredentialsScope scope, String id, String description, Secret secret) {
+        super(scope, id, description);
+        this.secret = secret;
+    }
+
+    public String getToken() {
+        return secret.getPlainText();
+    }
+
+    public Secret getSecret() {
+        return secret;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends BaseStandardCredentialsDescriptor {
+        @Override
+        public String getDisplayName() {
+            return "Alauda Token";
+        }
+    }
+
+}
